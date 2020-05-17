b88b19fa7243170dc20f3d69f8599189a51207d9
Li-Cheng Zheng <ny8859884@gmail.com>
2020-05-17 16:08:37 +0800

    [add] FM process

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "partitioner.h"
 using std::cout;
 using std::endl;
 
@@ -6,5 +7,10 @@ int main(int argc, char* argv[]){
     cout << "Here you can get the input/output filenames" << endl;
     for (int i = 0; i < argc; ++i)
         cout << "argv[" << i << "]: " << argv[i] << endl;
+    Partitioner partitioner;
+    partitioner.read_file(argv[1]);
+    partitioner.initialize();
+    partitioner.FM_algorithm();
+    partitioner.write_file(argv[2]);
     return 0;
 }
